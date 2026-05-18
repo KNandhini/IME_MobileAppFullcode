1b2ac903400a9089dfdfd422adfdbf08ee9d60fe
karthik0610s <karthikeyan.madhavan0610@gmail.com>
2026-05-18 12:06:39 +0530

    Activity and circular changes

--- source/repos/IME_Phase1/Backend/IME.API/Requests/AchievementUpdateRequest.cs
+++ source/repos/IME_Phase1/Backend/IME.API/Requests/AchievementUpdateRequest.cs
@@ -2,10 +2,11 @@ namespace IME.API.Requests;
 
 public class AchievementUpdateRequest
 {
-    public int MemberId { get; set; }
+    public string MemberName { get; set; } = string.Empty;
     public string Title { get; set; } = string.Empty;
     public string? Description { get; set; }
     public string? AchievementDate { get; set; }
     public IFormFile? Photo { get; set; }
     public IFormFile? Attachment { get; set; }
+    public int MemberId { get; internal set; }
 }
