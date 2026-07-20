672cd45fa6566217a84c64194469e72a40fefae1
priya anand <priya12294@gmailcom>
2026-07-20 11:07:20 +0530

    Institute change to Institution

--- source/repos/IME_Phase1/Frontend/src/screens/LoginScreen.js
+++ source/repos/IME_Phase1/Frontend/src/screens/LoginScreen.js
@@ -221,7 +221,7 @@ const LoginScreen = ({ navigation }) => {
           </View>
 
           <Text style={styles.footerText}>
-            © {new Date().getFullYear()} Institute of Municipal Engineers · All rights reserved
+            © {new Date().getFullYear()} Institution of Municipal Engineers · All rights reserved
           </Text>
         </ScrollView>
       </KeyboardAvoidingView>
